e4b7cb3c1a73ddeacf2fde065c9f44a0a184b899
racoon.cho <racoon.cho@kakaoenterprise.com>
2021-11-12 13:59:42 +0900

    add postcss + autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
+import { preprocess } from './svelte.config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte({ preprocess })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
